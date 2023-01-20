a35ef2ae823840ce842c42485f7b43111efa7b3a
Patricia Robles <patriciarobles.tech@gmail.com>
2023-01-20 15:41:03 +0000

    user input / README file added

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,24 +1,35 @@
 
-for (let i = 1; i <= 100; i++) {
-    let result = [];
 
-    if (i % 3 === 0) {
-        result.push('Fizz')
-    }
-    if (i % 5 === 0) {
-        result.push('Buzz')
-    }
+function fizzBuzz(n, w1, w2) {
+    for (let i = 1; i <= n; i++) {
+        let result = [];
 
-    if (i % 7 === 0) {
-        result.push('Bang')
-    }
+        if (i % 3 === 0) {
+            //result.push('Fizz')
+            result.push(w1)
+        }
 
-    if (i % 11 === 0) {
-        result.push('Boom')
-    }
+        if (i % 5 === 0) {
+            result.push('Buzz')
+        }
 
-    if (result.length === 0) {
-        result.push(i)
+        if (i % 7 === 0) {
+            result.push('Bang')
+        }
+
+        if (i % 11 === 0) {
+            result.push('Boom')
+        }
+
+        if (result.length === 0) {
+            result.push(i)
+        }
+        console.log(result.join(''))
     }
-    console.log(result.join(''))
 }
+
+const prompt = require("prompt-sync")();
+let userInput = prompt("Please enter a maximum number: ")
+let userFizz = prompt("Custom word for multiples of 3: ")
+
+fizzBuzz(userInput, userFizz)
---
+++ README.md
@@ -0,0 +1,4 @@
+# FizzBuzz
+JS implementation of the classic FizzBuzz exercise. Fizz, buzz... and a bit of extra logic.
+
+Practice flow control through the use of loops and conditional statements. Array manipulation and basic array methods. Allows for a small degree of user customisation.
